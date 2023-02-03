2d4acef7ab0b99f76df926dc646d2ba5acf4683e
Zhejie Jiang <zhejiej@nycstudents.net>
2023-02-03 09:14:11 -0500

    finish

--- src/GroceryStore.java
+++ src/GroceryStore.java
@@ -17,7 +17,13 @@ public class GroceryStore
      *  Precondition: min > 0 */
     public ArrayList<Integer> getReorderList(int min)
     {
-        /* to be implemented in part (a) */
+        ArrayList<Integer> reorderList = new ArrayList<Integer>();
+        for(int i = 0; i< productsStocked.length; i++)
+        {
+            Product product = productsStocked[i];
+            if(product.getQuantity() <= min){reorderList.add(i);}
+        }
+        return reorderList;
     }
 
     /** Returns true if all products named in shoppingList are available for purchase
@@ -27,6 +33,31 @@ public class GroceryStore
      */
     public boolean checkAvailability(ArrayList<String> shoppingList)
     {
-        /* to be implemented in part (b) */
+        for (String item: shoppingList)
+        {
+            for(Product product : productsStocked)
+            {
+                if(product.getName().equals(item))
+                {
+                    if(product.getQuantity() == 0){return false;}
+                }
+            }
+        }
+        return true;
     }
+
+    /** Returns an ArrayList containing all Products from productStocked that
+     *  have a weight that exceeds the “weight” value provided in the parameter
+     */
+    public ArrayList<Product> oversized(double weight)
+    {
+        ArrayList<Product> oversizedList = new ArrayList<Product>();
+        for(int i = 0; i< productsStocked.length; i++)
+        {
+            Product product = productsStocked[i];
+            if(product.getWeight() > weight){oversizedList.add(productsStocked[i]);}
+        }
+        return oversizedList;
+    }
+
 }
--- src/Product.java
+++ src/Product.java
@@ -2,11 +2,14 @@ public class Product
 {
     private String name;
     private int quantity;
+    private double weight;
 
-    public Product(String name, int quantity)
+    public Product(String name, int quantity, double weight)
     {
         this.name = name;
         this.quantity = quantity;
+        this.weight = weight;
+
     }
 
     /** Returns the name of the product */
@@ -21,4 +24,9 @@ public class Product
     {
         return quantity;
     }
+    public double getWeight()
+    {
+        return weight;
+    }
+
 }
